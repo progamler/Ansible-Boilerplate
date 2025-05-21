e48993e4f1c5a787466ce9b1039b0cb292870dc4
Jan-Philipp <github@guerilla-it.de>
2025-05-21 18:09:51 +0200

    Create dir.sh

---
+++ dir.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Top-level files
+touch production staging site.yml webservers.yml dbservers.yml
+
+# group_vars and host_vars
+mkdir -p group_vars host_vars
+touch group_vars/group1.yml group_vars/group2.yml
+touch host_vars/hostname1.yml host_vars/hostname2.yml
+
+# Optional directories
+mkdir -p library module_utils filter_plugins
+
+# Roles and subdirectories
+for role in common webtier monitoring fooapp; do
+    mkdir -p roles/$role/{tasks,handlers,templates,files,vars,defaults,meta,library,module_utils,lookup_plugins}
+    
+    # Create main.yml where applicable
+    touch roles/$role/tasks/main.yml
+    touch roles/$role/handlers/main.yml
+    touch roles/$role/vars/main.yml
+    touch roles/$role/defaults/main.yml
+    touch roles/$role/meta/main.yml
+
+    # Create example files/templates in 'common' role only
+    if [ "$role" = "common" ]; then
+        touch roles/$role/templates/ntp.conf.j2
+        touch roles/$role/files/bar.txt roles/$role/files/foo.sh
+    fi
+done
